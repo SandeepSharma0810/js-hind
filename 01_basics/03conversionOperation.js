22beabd215af403177f6044c1495c096beb0b47d
Sandeep Sharma <146651030+SandeepSharma0810@users.noreply.github.com>
2023-12-06 05:03:34 +0000

    oprations

--- 01_basics/03conversionOperation.js
+++ 01_basics/03conversionOperation.js
@@ -22,5 +22,27 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber) 
-console.log(typeof stringNumber);
+// console.log(stringNumber) 
+// console.log(typeof stringNumber);
+
+let value = 3
+let NegValue = -value
+// console.log(NegValue);
+// console.log(typeof NegValue);
+
+// console.log(2+2);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Sandeep"
+let str3 = str1 + str2
+// console.log(str3);
+// console.log(1 + "3")
+// console.log(1 + "4" + 5);
+// console.log(1+3+"4");
+console.log(true);
+console.log(+true)
+console.log(typeof true+2);
+console.log(+"")
+console.log("");
+// link to study https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
